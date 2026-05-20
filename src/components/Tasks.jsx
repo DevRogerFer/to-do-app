e84e4ab41e51f59f7d5491474156fb701c38ab4b
Rogério Fernandes <roferrock@gmail.com>
2026-05-19 21:54:53 -0300

    chore: project startup

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import Header from "./Header";
+
+function Tasks() {
+  const [inputValue, setInputValue] = useState();
+  const [messages, setMessages] = useState([
+    "Hello World",
+    "Welcome to React",
+    "Have a great day!",
+  ]);
+  function handleButtonClick() {
+    setMessages([...messages, inputValue]);
+  }
+
+  return (
+    <div>
+      <Header>
+        <h1>Add a task</h1>
+      </Header>
+
+      <input
+        className="input"
+        type="text"
+        placeholder="Create your task..."
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+
+      <button className="button" onClick={handleButtonClick}>
+        Add task
+      </button>
+
+      <Header>
+        <h1>My Tasks</h1>
+      </Header>
+
+      <div>
+        <ul>
+          {messages.map((message) => {
+            return <li>{message}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Tasks;
