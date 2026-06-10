f62e97eab3e9c93ec61486caf8ef8997acbe370e
Rogerio Fernandes <roferrock@gmail.com>
2026-06-09 23:19:13 -0300

    feat: improve input component states

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,18 @@
-const Input = ({ ...rest }) => {
+const Input = ({ label, className = "", id, ...rest }) => {
   return (
-    <input
-      className="rounded-lg border border-solid border-[#ECECEC] px-4 py-3"
-      {...rest}
-    />
+    <div className="flex flex-col items-start gap-2">
+      {label && (
+        <label className="text-sm font-medium text-[#35383E]" htmlFor={id}>
+          {label}
+        </label>
+      )}
+
+      <input
+        className={`w-full rounded-lg border border-solid border-[#ECECEC] px-4 py-3 text-sm text-[#35383E] outline-none placeholder:text-[#9A9C9E] focus:border-[#00ADB5] focus:ring-1 focus:ring-[#00ADB5] ${className}`}
+        id={id}
+        {...rest}
+      />
+    </div>
   )
 }
 
