9e49f404700d28d7c9ccdda9caeddedc3e15656c
Rogerio Fernandes <roferrock@gmail.com>
2026-06-13 12:20:07 -0300

    feat: ensure task can be added

--- src/components/TimeSelect.jsx
+++ src/components/TimeSelect.jsx
@@ -1,6 +1,6 @@
 import InputLabel from "./InputLabel"
 
-const TimeSelect = () => {
+const TimeSelect = (props) => {
   return (
     <div className="flex flex-col gap-1 text-left">
       <InputLabel htmlFor="time">Horário</InputLabel>
@@ -8,6 +8,7 @@ const TimeSelect = () => {
       <select
         id="time"
         className={`w-full rounded-lg border border-solid border-[#ECECEC] px-4 py-3 text-sm text-[#35383E] outline-none placeholder:text-[#9A9C9E] focus:border-[#00ADB5] focus:ring-1 focus:ring-[#00ADB5]`}
+        {...props}
       >
         <option value="morning">Manhã</option>
         <option value="afternoon">Tarde</option>
