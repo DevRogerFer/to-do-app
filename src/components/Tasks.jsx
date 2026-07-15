45f26d999cf8ca4de73d004e34b8c9cb43fd4724
Rogerio Fernandes <roferrock@gmail.com>
2026-07-15 19:59:43 -0300

    feat: ensure task status is update via api

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,46 +1,15 @@
-import { useQueryClient } from "@tanstack/react-query"
-import { toast } from "sonner"
-
 import { CloudSunIcon, MoonIcon, SunIcon } from "../assets/icons/"
 import { useGetTasks } from "../hooks/data/use-get-tasks"
-import { taskQueryKeys } from "../keys/queries"
 import Header from "./Header"
 import TaskItem from "./TaskItem"
 import TasksSeparator from "./TasksSeparator"
 
 const Tasks = () => {
-  const queryClient = useQueryClient()
   const { data: tasks } = useGetTasks()
 
   const morningTasks = tasks?.filter((task) => task.time === "morning")
   const afternoonTasks = tasks?.filter((task) => task.time === "afternoon")
   const eveningTasks = tasks?.filter((task) => task.time === "evening")
-
-  const handleTaskCheckboxClick = (taskId) => {
-    const newTasks = tasks.map((task) => {
-      if (task.id !== taskId) {
-        return task
-      }
-
-      if (task.status === "not_started") {
-        toast.success("Tarefa iniciada com sucesso!")
-        return { ...task, status: "in_progress" }
-      }
-
-      if (task.status === "in_progress") {
-        toast.success("Tarefa concluída com sucesso!")
-        return { ...task, status: "done" }
-      }
-
-      if (task.status === "done") {
-        toast.success("Tarefa reiniciada com sucesso!")
-        return { ...task, status: "not_started" }
-      }
-
-      return task
-    })
-    queryClient.setQueryData(taskQueryKeys.getAll(), newTasks)
-  }
 
   return (
     <div className="w-full space-y-6 px-8 py-16">
@@ -59,11 +28,7 @@ const Tasks = () => {
             )}
             {/* TAREFAS DA MANHÃ */}
             {morningTasks?.map((task) => (
-              <TaskItem
-                key={task.id}
-                task={task}
-                handleCheckboxClick={handleTaskCheckboxClick}
-              />
+              <TaskItem key={task.id} task={task} />
             ))}
           </div>
         </div>
@@ -78,11 +43,7 @@ const Tasks = () => {
           )}
           {/* TAREFAS DA TARDE */}
           {afternoonTasks?.map((task) => (
-            <TaskItem
-              key={task.id}
-              task={task}
-              handleCheckboxClick={handleTaskCheckboxClick}
-            />
+            <TaskItem key={task.id} task={task} />
           ))}
         </div>
 
@@ -96,11 +57,7 @@ const Tasks = () => {
           )}
           {/* TAREFAS DA NOITE */}
           {eveningTasks?.map((task) => (
-            <TaskItem
-              key={task.id}
-              task={task}
-              handleCheckboxClick={handleTaskCheckboxClick}
-            />
+            <TaskItem key={task.id} task={task} />
           ))}
         </div>
       </div>
